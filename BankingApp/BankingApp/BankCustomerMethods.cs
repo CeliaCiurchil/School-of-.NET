8cab5cd59272440dc38d2cba422ae587bba985c0
Ciurchil-Giurgiu Celia <Celia.Ciurchil@endava.com>
2025-10-14 12:13:38 +0300

    completing partial class BankingApp with methods for customer name

---
+++ BankingApp/BankingApp/BankCustomerMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp
+{
+    public partial class BankCustomer
+    {
+        public string ReturnFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+
+
+        // Method to update the customer's name
+        public void UpdateName(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+
+        // Method to display customer information
+        public string DisplayCustomerInfo()
+        {
+            return $"Customer ID: {CustomerID}, Name: {ReturnFullName()}";
+        }
+
+    }
+}
