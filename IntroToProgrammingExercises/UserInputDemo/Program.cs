b07ba000d616439de3a0d427f8402313538f11f5
Ciurchil-Giurgiu Celia <Celia.Ciurchil@endava.com>
2025-10-15 10:23:51 +0300

    DateTime manipulation exercises

--- IntroToProgrammingExercises/UserInputDemo/Program.cs
+++ IntroToProgrammingExercises/UserInputDemo/Program.cs
@@ -1,11 +1,15 @@
-﻿int age;
+﻿using System.Globalization;
+
+int age;
 char gender;
 int retirementAge;
 
+DateOnly dob = new DateOnly();
 
+Console.Write("Enter your date of birth: (dd/mm/yyyy)");
+dob = DateOnly.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+age = DateTime.Now.Year - dob.Year;
 
-Console.Write("Enter your age: ");
-age = int.Parse(Console.ReadLine());
 
 Console.Write("Enter your gender (M/F): ");
 gender = char.Parse(Console.ReadLine());
@@ -14,8 +18,8 @@ Console.Write("Enter your retirement age: ");
 retirementAge = int.Parse(Console.ReadLine());
 
 int workingYearsLeft = retirementAge - age;
-
-Console.WriteLine($"You have {workingYearsLeft} working years left until retirement\n");
+int estimatedRetirementDate = DateTime.Now.AddYears(workingYearsLeft).Year;
+Console.WriteLine($"You have {workingYearsLeft} working years left until retirement\n Year {estimatedRetirementDate}");
 double num=1.3;
 int num2=2;
 double num3 = num+num2;
