d9da03f435bfaa0bf01ea9ba967bea00b30b5ea1
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 16:18:01 +0200

    test for factory

---
+++ CafeConsole/Cafe.Tests/Factories/BeverageFactoryTests.cs
@@ -0,0 +1,42 @@
+﻿using Cafe.Domain.Beverages;
+using Cafe.Domain.Beverages.Interfaces;
+using Cafe.Domain.Enums;
+using Cafe.Infrastructure.Factories;
+using System;
+using Xunit;
+
+namespace Cafe.Tests.Factories
+{
+    public class BeverageFactoryTests
+    {
+        [Fact]
+        public void CreateBeverage_WithExpressoType_ReturnsExpressoInstance()
+        {
+            var factory = new BeverageFactory();
+
+            IBeverage beverage = factory.CreateBeverage(BeverageType.Expresso);
+
+            Assert.IsType<Expresso>(beverage);
+        }
+
+        [Fact]
+        public void CreateBeverage_WithTeaType_ReturnsTeaInstance()
+        {
+            var factory = new BeverageFactory();
+
+            IBeverage beverage = factory.CreateBeverage(BeverageType.Tea);
+
+            Assert.IsType<Tea>(beverage);
+        }
+
+        [Fact]
+        public void CreateBeverage_WithInvalidType_ThrowsArgumentException()
+        {
+            var factory = new BeverageFactory();
+
+            var invalidType = (BeverageType)999;
+
+            Assert.Throws<ArgumentException>(() => factory.CreateBeverage(invalidType));
+        }
+    }
+}
