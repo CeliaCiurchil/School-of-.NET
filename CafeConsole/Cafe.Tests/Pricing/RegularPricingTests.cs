dd3f54fc8a8d838f01374ab9b912d32f8e7d9b1e
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 16:20:11 +0200

    tests for pricing strategies

---
+++ CafeConsole/Cafe.Tests/Pricing/RegularPricingTests.cs
@@ -0,0 +1,19 @@
+﻿using Cafe.Domain.Pricing;
+using Xunit;
+
+namespace Cafe.Tests.Pricing
+{
+    public class RegularPricingTests
+    {
+        [Fact]
+        public void Apply_WithTenDollars_ReturnsSameAmount()
+        {
+            IPricingStrategy pricing = new RegularPricing();
+            decimal subtotal = 10.00m;
+
+            decimal total = pricing.Apply(subtotal);
+
+            Assert.Equal(10.00m, total);
+        }
+    }
+}
---
+++ CafeConsole/Cafe.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Cafe.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
