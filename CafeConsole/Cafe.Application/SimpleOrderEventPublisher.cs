60fba7dd71c04786794d642941298d3a5ec8ce68
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 09:33:45 +0200

    implementation of order publisher

---
+++ CafeConsole/Cafe.Application/SimpleOrderEventPublisher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Cafe.Domain.Events;
+namespace Cafe.Application;
+
+internal class SimpleOrderEventPublisher : IOrderEventPublisher
+{
+    private List<IOrderEventSubscriber> subscribers = new();
+
+    public void Publish(OrderPlaced orderPlaced)
+    {
+        foreach (var subscriber in subscribers)
+        {
+            subscriber.On(orderPlaced);
+        }
+    }
+
+    public void Subscribe(IOrderEventSubscriber subscriber)
+    {
+        subscribers.Add(subscriber);
+    }
+
+    public void Unsubscribe(IOrderEventSubscriber subscriber)
+    {
+        subscribers.Remove(subscriber);
+    }
+}
