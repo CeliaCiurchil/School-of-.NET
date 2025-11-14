cfd2f232fee63a6affdc8c71b19ff6740ebf7577
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 16:17:45 +0200

    tests for adding decorators to coffee

---
+++ CafeConsole/Cafe.Tests/Beverages/Decorators/ExtraShotDecoratorTests.cs
@@ -0,0 +1,35 @@
+﻿using Cafe.Domain.Beverages;
+using Cafe.Domain.Beverages.Decorators;
+using Cafe.Domain.Beverages.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Tests.Beverages.Decorators
+{
+    public class ExtraShotDecoratorTests
+    {
+        [Fact]
+        public void Cost_ExpressoWithExtraShot_IsBasePlusExtraShot()
+        {
+            IBeverage baseDrink = new Expresso();          
+            IBeverage withExtraShot = new ExtraShotDecorator(baseDrink); 
+
+            decimal cost = withExtraShot.Cost();
+
+            Assert.Equal(3.30m, cost); 
+        }
+
+        [Fact]
+        public void Describe_ExpressoWithExtraShot_AppendsExtraShot()
+        {
+            IBeverage withExtraShot = new ExtraShotDecorator(new Expresso());
+
+            string description = withExtraShot.Describe();
+
+            Assert.Contains("extra shot", description.ToLowerInvariant());
+        }
+    }
+}
