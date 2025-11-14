42937e2555ea3101799573b792d299ec3b16ceaa
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 09:28:31 +0200

    base class for decorators

---
+++ CafeConsole/Cafe.Domain/Beverages/Decorators/BeverageDecorator.cs
@@ -0,0 +1,21 @@
+﻿using Cafe.Domain.Beverages.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Domain.Beverages.Decorators
+{
+    public abstract class BeverageDecorator : IBeverage
+    {
+        protected readonly IBeverage _beverage;
+
+        protected BeverageDecorator(IBeverage beverage)
+            => _beverage = beverage ?? throw new ArgumentNullException(nameof(beverage));
+
+        public virtual string Name => _beverage.Name;
+        public virtual decimal Cost() => _beverage.Cost();
+        public virtual string Describe() => _beverage.Describe();
+    }
+}
