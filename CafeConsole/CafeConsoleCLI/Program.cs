4cd48f2bc95a6c5bf76946e6343af92ab951c3ab
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 14:58:58 +0200

    Dependency injection using  Microsoft.Extensions.DependencyInjection

--- CafeConsole/CafeConsoleCLI/Program.cs
+++ CafeConsole/CafeConsoleCLI/Program.cs
@@ -1,3 +1,34 @@
-﻿using Cafe.CLI.Menus;
+﻿using Cafe.Application;
+using Cafe.Application.Services;
+using Cafe.CLI.Menus;
+using Cafe.Domain.Events;
+using Cafe.Domain.Factories;
+using Cafe.Domain.Pricing;
+using Cafe.Infrastructure.Factories;
+using Cafe.Infrastructure.Observers;
+using Microsoft.Extensions.DependencyInjection;
 
-Menu.Run();
+var services = new ServiceCollection();
+
+services.AddSingleton<IBeverageFactory, BeverageFactory>();
+
+services.AddSingleton<RegularPricing>();
+services.AddSingleton<HappyHourPricing>();
+services.AddSingleton<IPricingStrategyManager, PricingManager>();
+
+services.AddSingleton<IOrderEventPublisher, SimpleOrderEventPublisher>();
+services.AddSingleton<IOrderEventSubscriber, ConsoleOrderLogger>();
+services.AddSingleton<IOrderEventSubscriber, InMemoryOrderAnalytics>();
+
+services.AddSingleton<IOrderService, OrderService>();
+
+services.AddSingleton<OrderMenu>();
+services.AddSingleton<Menu>();
+
+var sp = services.BuildServiceProvider();
+
+var publisher = sp.GetRequiredService<IOrderEventPublisher>();
+foreach (var sub in sp.GetServices<IOrderEventSubscriber>())
+    publisher.Subscribe(sub);
+
+sp.GetRequiredService<Menu>().Run();
