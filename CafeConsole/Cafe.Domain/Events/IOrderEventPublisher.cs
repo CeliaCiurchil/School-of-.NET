dba05f817a84384d920bebecea1a9ba154392ce3
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 09:33:09 +0200

    interfaces for publisher and subscribers

---
+++ CafeConsole/Cafe.Domain/Events/IOrderEventPublisher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Domain.Events
+{
+    public interface IOrderEventPublisher
+    {
+        public void Publish(OrderPlaced orderPlaced);
+        public void Subscribe(IOrderEventSubscriber subscriber);
+        public void Unsubscribe(IOrderEventSubscriber subscriber);
+    }
+}
