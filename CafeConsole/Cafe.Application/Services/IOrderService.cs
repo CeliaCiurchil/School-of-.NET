6d4022509ff6df6b64330c56a039061b0ff9c672
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 14:52:54 +0200

    made interface for service for easier DI

---
+++ CafeConsole/Cafe.Application/Services/IOrderService.cs
@@ -0,0 +1,17 @@
+﻿using Cafe.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Cafe.Domain.Enums;
+using Cafe.Application.Models.Orders;
+
+namespace Cafe.Application.Services
+{
+    public interface IOrderService
+    {
+        OrderPlaced PlaceOrder(OrderRequest orderRequest);
+    }
+}
