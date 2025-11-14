dd0adb920cb43117b3ba7e1256d3e48316fa0511
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 09:31:53 +0200

    beverage factory implementation in infrastructure

---
+++ CafeConsole/Cafe.Infrastructure/Factories/BeverageFactory.cs
@@ -0,0 +1,29 @@
+﻿using Cafe.Domain.Beverages;
+using Cafe.Domain.Beverages.Interfaces;
+using Cafe.Domain.Enums;
+using Cafe.Domain.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Cafe.Infrastructure.Factories;
+
+public class BeverageFactory : IBeverageFactory
+{
+    public IBeverage CreateBeverage(BeverageType beverageType)
+    {
+        switch (beverageType)
+        {
+            case BeverageType.Expresso:
+                return new Expresso();
+            case BeverageType.Tea:
+                return new Tea();
+            case BeverageType.Choc:
+                return new HotChocolate();
+            default:
+                throw new ArgumentException("Invalid beverage type", nameof(beverageType));
+        }
+    }
+}
+
