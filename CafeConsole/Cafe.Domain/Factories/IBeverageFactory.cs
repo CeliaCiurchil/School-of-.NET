ceb7ea6ade46eef2dd1359753a005770a08e2f61
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 09:31:26 +0200

    interface for beverage factory

---
+++ CafeConsole/Cafe.Domain/Factories/IBeverageFactory.cs
@@ -0,0 +1,16 @@
+﻿using Cafe.Domain.Beverages;
+using Cafe.Domain.Beverages.Interfaces;
+using Cafe.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Domain.Factories
+{
+    public interface IBeverageFactory
+    {
+        public IBeverage CreateBeverage(BeverageType beverageType);
+    }
+}
