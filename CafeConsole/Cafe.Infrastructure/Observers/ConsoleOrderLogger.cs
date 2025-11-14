7cafad8ee9f28c5256a05951f4c6be31e9ddcc90
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 09:34:24 +0200

    the console logger subscriber implementation

---
+++ CafeConsole/Cafe.Infrastructure/Observers/ConsoleOrderLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Cafe.Domain.Events;
+namespace Cafe.Infrastructure.Observers;
+
+internal class ConsoleOrderLogger : IOrderEventSubscriber
+{
+    public void On(OrderPlaced evt)
+    {
+        Console.WriteLine($"""
+                Order {evt.OrderId} {evt.PlacedAt}
+                Items: {evt.Description}
+                Subtotal: {evt.Subtotal.ToString("C",CultureInfo.CurrentCulture)}
+                Pricing: {evt.PricingType}
+                Total: {evt.Total.ToString("C",CultureInfo.CurrentCulture)}
+            """);
+    }
+}
