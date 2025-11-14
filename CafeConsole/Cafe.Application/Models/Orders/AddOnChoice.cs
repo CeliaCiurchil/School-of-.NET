3862b72127e9721b91e6e0aa989babc22d8436ff
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 14:54:58 +0200

    dto s for easier data trasnfer to service

---
+++ CafeConsole/Cafe.Application/Models/Orders/AddOnChoice.cs
@@ -0,0 +1,11 @@
+﻿using Cafe.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Application.Models.Orders
+{
+    public sealed record AddOnChoice(AddOn Name, string? Option = null);
+}
---
+++ CafeConsole/Cafe.Application/Models/Orders/OrderRequest.cs
@@ -0,0 +1,16 @@
+﻿using Cafe.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Application.Models.Orders;
+
+public sealed record OrderRequest(
+    BeverageType BaseDrink,
+    IReadOnlyList<AddOnChoice> AddOns,
+    PricingType PricingPolicy,
+    string Currency = "$"
+);
+
