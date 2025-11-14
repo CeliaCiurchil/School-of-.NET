c46859a92c52add66f476eb4d6945962e020fbcc
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 09:35:09 +0200

    Order Service implementation

---
+++ CafeConsole/Cafe.Application/Services/OrderService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Application.Services;
+using Cafe.Domain.Enums;
+using Cafe.Domain.Factories;
+
+public class OrderService
+{
+    IBeverageFactory _beverageFactory;
+    public OrderService(IBeverageFactory beverageFactory)
+    {
+        _beverageFactory = beverageFactory;
+    }
+    public void ChooseBase(BeverageType beverageType)
+    {
+        var beverage = _beverageFactory.CreateBeverage(beverageType);
+        Console.WriteLine(beverage.Describe());
+    }
+}
