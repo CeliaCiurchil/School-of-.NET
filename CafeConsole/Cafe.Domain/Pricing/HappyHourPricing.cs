1a9ea7e8c70d3818d199e50e43b09ab2e343c90e
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 09:30:53 +0200

    implementation for the two pricing strategies

---
+++ CafeConsole/Cafe.Domain/Pricing/HappyHourPricing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Domain.Pricing
+{
+    internal class HappyHourPricing : IPricingStrategy
+    {
+        private const decimal DiscountRate = 0.20m;
+        public decimal Apply(decimal subtotal)
+        {
+            if(subtotal<0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(subtotal), "Subtotal cannot be negative.");
+            }
+
+            decimal discount = DiscountRate * subtotal;
+            decimal total = subtotal - discount;
+
+            return total;
+        }
+    }
+}
