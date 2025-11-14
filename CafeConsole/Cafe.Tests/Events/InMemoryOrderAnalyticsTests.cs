b29841696acfd541dbfbfa7e6a000d53cf27f654
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-14 16:19:15 +0200

    tested in memory analytics

---
+++ CafeConsole/Cafe.Tests/Events/InMemoryOrderAnalyticsTests.cs
@@ -0,0 +1,25 @@
+﻿using Cafe.Domain.Enums;
+using Cafe.Domain.Events;
+using Cafe.Infrastructure.Observers;
+using System;
+using Xunit;
+
+namespace Cafe.Tests.Events
+{
+    public class InMemoryOrderAnalyticsTests
+    {
+        [Fact]
+        public void On_WhenTwoOrdersPublished_AccumulatesTotalsCorrectly()
+        {
+            var analytics = new InMemoryOrderAnalytics();
+            var order1 = new OrderPlaced(Guid.NewGuid(), DateTimeOffset.Now, "Espresso", 3.50m, PricingType.Regular, 3.50m);
+            var order2 = new OrderPlaced(Guid.NewGuid(), DateTimeOffset.Now, "Tea", 2.00m, PricingType.Regular, 2.00m);
+
+            analytics.On(order1);
+            analytics.On(order2);
+
+            Assert.Equal(2, analytics.TotalOrders);
+            Assert.Equal(5.50m, analytics.TotalRevenue);
+        }
+    }
+}
