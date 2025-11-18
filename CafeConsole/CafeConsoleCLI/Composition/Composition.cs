6f14b731185f48b0c54430c4c3bde9baf0d0d33b
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-11-18 16:09:19 +0200

    moved composition into a different file to keep program.cs clean

---
+++ CafeConsole/CafeConsoleCLI/Composition/Composition.cs
@@ -0,0 +1,45 @@
+﻿using Cafe.Application;
+using Cafe.Application.Services;
+using Cafe.CLI.Menus;
+using Cafe.Domain.Events;
+using Cafe.Domain.Factories;
+using Cafe.Domain.Pricing;
+using Cafe.Infrastructure.Factories;
+using Cafe.Infrastructure.Observers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CafeConsoleCLI.Composition;
+
+public static class Composition
+{
+    public static IServiceProvider Build()
+    {
+        var services = new ServiceCollection();
+
+        services.AddSingleton<IBeverageFactory, BeverageFactory>();
+
+        services.AddSingleton<RegularPricing>();
+        services.AddSingleton<HappyHourPricing>();
+        services.AddSingleton<IPricingStrategyManager, PricingManager>();
+
+        services.AddSingleton<IOrderEventPublisher, SimpleOrderEventPublisher>();
+        services.AddSingleton<IOrderEventSubscriber, ConsoleOrderLogger>();
+        services.AddSingleton<IOrderEventSubscriber, InMemoryOrderAnalytics>();
+
+        services.AddSingleton<IOrderService, OrderService>();
+
+        services.AddSingleton<OrderMenu>();
+        services.AddSingleton<Menu>();
+
+        var sp = services.BuildServiceProvider();
+        ConnectSubscribers(sp);
+        return sp;
+    }
+
+    private static void ConnectSubscribers(IServiceProvider sp)
+    {
+        var publisher = sp.GetRequiredService<IOrderEventPublisher>();
+        foreach (var sub in sp.GetServices<IOrderEventSubscriber>())
+            publisher.Subscribe(sub);
+    }
+}
--- CafeConsole/CafeConsoleCLI/Program.cs
+++ CafeConsole/CafeConsoleCLI/Program.cs
@@ -1,34 +1,6 @@
-﻿using Cafe.Application;
-using Cafe.Application.Services;
-using Cafe.CLI.Menus;
-using Cafe.Domain.Events;
-using Cafe.Domain.Factories;
-using Cafe.Domain.Pricing;
-using Cafe.Infrastructure.Factories;
-using Cafe.Infrastructure.Observers;
+﻿using Cafe.CLI.Menus;
+using CafeConsoleCLI.Composition;
 using Microsoft.Extensions.DependencyInjection;
 
-var services = new ServiceCollection();
-
-services.AddSingleton<IBeverageFactory, BeverageFactory>();
-
-services.AddSingleton<RegularPricing>();
-services.AddSingleton<HappyHourPricing>();
-services.AddSingleton<IPricingStrategyManager, PricingManager>();
-
-services.AddSingleton<IOrderEventPublisher, SimpleOrderEventPublisher>();
-services.AddSingleton<IOrderEventSubscriber, ConsoleOrderLogger>();
-services.AddSingleton<IOrderEventSubscriber, InMemoryOrderAnalytics>();
-
-services.AddSingleton<IOrderService, OrderService>();
-
-services.AddSingleton<OrderMenu>();
-services.AddSingleton<Menu>();
-
-var sp = services.BuildServiceProvider();
-
-var publisher = sp.GetRequiredService<IOrderEventPublisher>();
-foreach (var sub in sp.GetServices<IOrderEventSubscriber>())
-    publisher.Subscribe(sub);
-
+var sp = Composition.Build();
 sp.GetRequiredService<Menu>().Run();
