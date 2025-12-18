f1fe7bdff7c612ecf91fb95918ecb857e72a9d87
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-12-18 11:30:03 +0200

    Interface for flight repository

---
+++ AirportTool/AirportTool.Application/Contracts/IFlightRepository.cs
@@ -0,0 +1,13 @@
+﻿using AirportTool.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTool.Application.Contracts
+{
+    public interface IFlightRepository : IGenericRepository<Flight>
+    {
+    }
+}
