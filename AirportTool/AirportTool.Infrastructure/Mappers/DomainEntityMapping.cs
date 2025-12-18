b74ca044f3d43172850f2ebd7ef7bb9aece975ee
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-12-18 11:40:43 +0200

    mapper from EF Core generated entity to domain

---
+++ AirportTool/AirportTool.Infrastructure/Mappers/DomainEntityMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Address = AirportTool.Domain.Entities.Address;
+using AddressDb = AirportTool.Infrastructure.Persistence.Entities.Address;
+
+using Flight = AirportTool.Domain.Entities.Flight;
+using FlightDb = AirportTool.Infrastructure.Persistence.Entities.Flight;
+
+namespace AirportTool.Infrastructure.Mappers
+{
+    public class DomainEntityMapping : Profile
+    {
+        public DomainEntityMapping()
+        {
+            CreateMap<Address, AddressDb>().ReverseMap();
+            CreateMap<Flight, FlightDb>().ReverseMap();
+        }
+    }
+}
