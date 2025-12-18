893230e35a2652e477159751db001e72530e3e8b
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-12-18 11:28:58 +0200

    Interface for generic repository

---
+++ AirportTool/AirportTool.Application/Contracts/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTool.Application.Contracts
+{
+    public interface IGenericRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken ct = default);
+        Task<T> GetByIdAsync(int id, CancellationToken ct = default);
+        Task AddAsync(T entity, CancellationToken ct = default);
+        Task UpdateAsync(T entity, CancellationToken ct = default);
+        Task DeleteAsync(int id, CancellationToken ct = default);
+        Task<bool> ExistsAsync(int id, CancellationToken ct = default);
+    }
+}
