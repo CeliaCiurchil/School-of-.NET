3eb7f3f641f0d6497aa24e747069d689540f8be6
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-12-18 11:41:05 +0200

    Implmentation of UnitOfWork

---
+++ AirportTool/AirportTool.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using AirportTool.Application.Contracts;
+using AirportTool.Infrastructure.Persistence;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTool.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly FlightBookingDbContext _context;
+
+        public IFlightRepository Flights { get; }
+
+        public UnitOfWork(FlightBookingDbContext context, IMapper mapper, IFlightRepository flightRepository)
+        {
+            _context = context;
+            Flights = flightRepository;
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
+
