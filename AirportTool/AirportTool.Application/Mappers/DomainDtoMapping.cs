96d77ac7a13445e28aaae477e05fbfefd50e7c80
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-12-18 11:37:27 +0200

    mapper from domain model to Dto

---
+++ AirportTool/AirportTool.Application/Mappers/DomainDtoMapping.cs
@@ -0,0 +1,22 @@
+﻿using AirportTool.Application.ModelDto.Flight;
+using AirportTool.Domain.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTool.Application.Mappers
+{
+    public class DomainDtoMapping : Profile
+    {
+        public DomainDtoMapping()
+        {
+            CreateMap<Flight,FlightReadDto>().ReverseMap();
+            CreateMap<Flight,FlightCreateDto>().ReverseMap();
+            CreateMap<Flight,FlightUpdateDto>().ReverseMap();
+
+        }
+    }
+}
