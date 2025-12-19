7d29467d1fde399e8a071cf4006e7d0227b3f492
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-12-19 13:28:54 +0200

    new domain models

---
+++ AirportTool/AirportTool.Domain/Entities/FlightSchedule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AirportTool.Domain.Entities
+{
+    public class FlightSchedule
+    {
+        public int Id { get; set; }
+        public int FlightId { get; set; }
+        public DateTime ScheduledDepartureUtc { get; set; }
+        public DateTime ScheduledArrivalUtc { get; set; }
+        public int? GateId { get; set; }
+        public int? AssignedAircraftId { get; set; }
+        public int StatusId { get; set; }
+
+        public Gate? Gate { get; set; }
+        public Aircraft? AssignedAircraft { get; set; }
+        public FlightStatus Status { get; set; } = null!;
+    }
+}
