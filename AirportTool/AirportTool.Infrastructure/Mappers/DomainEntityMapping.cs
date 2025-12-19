3f1aeaf8e441147cebc2c51cf3fa4b2e4f293994
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-12-19 13:29:54 +0200

    entitiy to domain mapper

--- AirportTool/AirportTool.Infrastructure/Mappers/DomainEntityMapping.cs
+++ AirportTool/AirportTool.Infrastructure/Mappers/DomainEntityMapping.cs
@@ -4,6 +4,14 @@ using AddressDb = AirportTool.Infrastructure.Persistence.Entities.Address;
 
 using Flight = AirportTool.Domain.Entities.Flight;
 using FlightDb = AirportTool.Infrastructure.Persistence.Entities.Flight;
+using FlightSchedule = AirportTool.Domain.Entities.FlightSchedule;
+using FlightScheduleDb = AirportTool.Infrastructure.Persistence.Entities.FlightSchedule;
+using Gate = AirportTool.Domain.Entities.Gate;
+using GateDb = AirportTool.Infrastructure.Persistence.Entities.Gate;
+using Aircraft = AirportTool.Domain.Entities.Aircraft;
+using AircraftDb = AirportTool.Infrastructure.Persistence.Entities.Aircraft;
+using FlightStatus = AirportTool.Domain.Entities.FlightStatus;
+using FlightStatusDb = AirportTool.Infrastructure.Persistence.Entities.FlightStatus;
 
 namespace AirportTool.Infrastructure.Mappers
 {
@@ -13,6 +21,10 @@ namespace AirportTool.Infrastructure.Mappers
         {
             CreateMap<Address, AddressDb>().ReverseMap();
             CreateMap<Flight, FlightDb>().ReverseMap();
+            CreateMap<FlightSchedule, FlightScheduleDb>().ReverseMap();
+            CreateMap<Gate, GateDb>().ReverseMap();
+            CreateMap<Aircraft, AircraftDb>().ReverseMap();
+            CreateMap<FlightStatus, FlightStatusDb>().ReverseMap();
         }
     }
 }
