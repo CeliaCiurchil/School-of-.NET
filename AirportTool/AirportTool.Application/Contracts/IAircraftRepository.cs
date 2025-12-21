05c35025940b743dbc46248e98bf489efe6a85d0
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-12-21 17:45:42 +0200

    introduced repos for aircraft, airline and airport

---
+++ AirportTool/AirportTool.Application/Contracts/IAircraftRepository.cs
@@ -0,0 +1,14 @@
+﻿using AirportTool.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTool.Application.Contracts
+{
+    public interface IAircraftRepository : IGenericRepository<Aircraft>
+    {
+        Task<Aircraft> GetByTailNumberAsync(string tailNumber);
+    }
+}
