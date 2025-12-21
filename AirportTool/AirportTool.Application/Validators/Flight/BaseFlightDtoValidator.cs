2c1037a1566142e2db1c49efa672432068372661
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-12-21 17:48:19 +0200

    new validator for new dto

--- AirportTool/AirportTool.Application/Validators/Flight/BaseFlightDtoValidator.cs
+++ AirportTool/AirportTool.Application/Validators/Flight/BaseFlightDtoValidator.cs
@@ -7,28 +7,39 @@ namespace AirportTool.Application.Validators.Flight
     {
         public BaseFlightDtoValidator()
         {
-            RuleFor(x => x.AirlineId)
-                .GreaterThan(0);
+            RuleFor(x => x.AirlineIata)
+                .NotEmpty()
+                .Length(2)
+                .Matches("^[A-Z]{2}$")
+                .WithMessage("AirlineIata must be exactly 2 uppercase letters (e.g., RO).");
+
+            RuleFor(x => x.OriginIata)
+                .NotEmpty()
+                .Length(3)
+                .Matches("^[A-Z]{3}$")
+                .WithMessage("OriginIata must be exactly 3 uppercase letters (e.g., OTP).");
+
+            RuleFor(x => x.DestinationIata)
+                .NotEmpty()
+                .Length(3)
+                .Matches("^[A-Z]{3}$")
+                .WithMessage("DestinationIata must be exactly 3 uppercase letters (e.g., LHR).");
+
+            RuleFor(x => x)
+                .Must(x => x.OriginIata != x.DestinationIata)
+                .WithMessage("OriginIata and DestinationIata must be different.");
 
             RuleFor(x => x.FlightNumber)
                 .NotEmpty()
                 .MaximumLength(8)
                 .Matches("^[A-Za-z]+[0-9]+$")
-                .WithMessage("FlightNumber must contain letters followed by digits.");
+                .WithMessage("FlightNumber must be letters+digits (e.g., RO391) and max 8 chars.");
 
-            RuleFor(x => x.OriginAirportId)
-                .GreaterThan(0);
-
-            RuleFor(x => x.DestinationAirportId)
-                .GreaterThan(0);
-
-            RuleFor(x => x)
-                .Must(x => x.OriginAirportId != x.DestinationAirportId)
-                .WithMessage("OriginAirportId must be different from DestinationAirportId.");
-
-            RuleFor(x => x.DefaultAircraftId)
-                .Must(id => id is null || id > 0)
-                .WithMessage("DefaultAircraftId must be greater than 0 when provided.");
+            RuleFor(x => x.DefaultAircraftTail)
+                .MaximumLength(10)
+                .Matches("^[A-Z0-9-]+$")
+                .When(x => !string.IsNullOrWhiteSpace(x.DefaultAircraftTail))
+                .WithMessage("DefaultAircraftTail contains invalid characters.");
         }
     }
 }
