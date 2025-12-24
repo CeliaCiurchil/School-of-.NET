fa70f735578433c6e24768c39c93b0b5970ab58b
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-12-24 13:59:52 +0200

    Add pricing service with basic fare calculation logic so client doenst decide business related prices

---
+++ AirportTool/AirportTool.Application/Contracts/IPricingService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTool.Application.Contracts
+{
+    public interface IPricingService
+    {
+        Task<PriceBreakdown> PriceTicketAsync(PriceTicketRequest request, CancellationToken ct = default);
+    }
+
+    public sealed record PriceTicketRequest(
+        char FareClass
+    );
+    public sealed record PriceBreakdown(
+        decimal BasePrice,
+        decimal Taxes,
+        decimal TotalPrice,
+        string Currency,
+        bool IsRefundable
+    );
+}
---
+++ AirportTool/AirportTool.Application/Services/PricingService.cs
@@ -0,0 +1,38 @@
+﻿using AirportTool.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTool.Application.Services
+{
+    public class PricingService : IPricingService
+    {
+            public Task<PriceBreakdown> PriceTicketAsync(PriceTicketRequest request, CancellationToken ct = default)
+            {
+                // super basic rules: base by class, taxes = 10% of base + fixed fee
+                var fareClass = char.ToUpperInvariant(request.FareClass);
+                var basePrice = fareClass switch
+                {
+                    'Y' => 100m,
+                    'M' => 150m,
+                    'J' => 300m,
+                    'F' => 500m,
+                    _ => 120m
+                };
+
+                var taxes = Math.Round(basePrice * 0.10m + 15m, 2, MidpointRounding.AwayFromZero);
+                var total = Math.Round(basePrice + taxes, 2, MidpointRounding.AwayFromZero);
+
+                return Task.FromResult(new PriceBreakdown(
+                    BasePrice: basePrice,
+                    Taxes: taxes,
+                    TotalPrice: total,
+                    Currency: "EUR",
+                    IsRefundable: fareClass is 'J' or 'F'
+                ));
+            }
+        
+    }
+}
